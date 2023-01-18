af4905495d5cd98baa50b0aadb013e0b601480d6
Medmixal <medmixal@yahoo.com>
2023-01-18 11:11:29 +0400

    finish 2 step

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,11 @@
+let main = document.querySelector("main");
+cats.forEach(function (cat) {
+  let card = `<div class="${cat.favourite ? "card like" : "card"}" style="background-image: url(${cat.img_link})">
+    <span>${cat.name}</span>
+    </div>`;
+  main.innerHTML += card;
+});
+
 let cards = document.getElementsByClassName("card");
 for (let i = 0, cnt = cards.length; i < cnt; i++) {
   const width = cards[i].offsetWidth;
