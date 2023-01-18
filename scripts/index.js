4768096d335919e393b0072d9db3b84a2f3e731a
Medmixal <medmixal@yahoo.com>
2023-01-18 11:34:06 +0400

    finish 4 step

--- scripts/index.js
+++ scripts/index.js
@@ -26,3 +26,39 @@ closePopupForm.addEventListener("click", () => {
   popupForm.classList.remove("active");
   popupForm.parentElement.classList.remove("active");
 })
+
+const api = new Api("medmixal"); 
+let form = document.forms[0];
+form.img_link.addEventListener("change", (e) => {
+  form.firstElementChild.style.backgroundImage = `url(${e.target.value})`
+})
+form.img_link.addEventListener("input", (e) => {
+  form.firstElementChild.style.backgroundImage = `url(${e.target.value})`
+})
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  let body = {};
+  for (let i = 0; i < form.elements.length; i++) {
+    let inp = form.elements[i];
+    if (inp.type === "checkbox") {
+      body[inp.name] = inp.checked;
+    } else if (inp.name && inp.value) {
+      if (inp.type === "number") {
+        body[inp.name] = +inp.value;
+      } else {
+        body[inp.name] = inp.value;
+      }
+    }
+  }
+  console.log(body);
+  api.addCat(body)
+    .then(res => res.json())
+    .then(data => {
+      if (data.message === "ok") {
+        form.reset();
+        closePopupForm.click();
+      } else {
+        console.log(data);
+      }
+    })
+})
