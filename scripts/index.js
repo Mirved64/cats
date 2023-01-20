17e1d298f4ad24b6b31a5615052fc35428f752dc
Medmixal <medmixal@yahoo.com>
2023-01-20 10:28:50 +0400

    coderevie

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,73 @@
+let main = document.querySelector("main");
+
+const api = new Api("medmixal");
+
+
+// Генерация карточек из файла 
+/* cats.forEach(function (cat) {
+  let card = `<div class="${cat.favourite ? "card like" : "card"}" style="background-image: url(${cat.img_link})">
+    <span>${cat.name}</span>
+    </div>`;
+  main.innerHTML += card;
+}); */
+
+
+/* let cards = document.getElementsByClassName("card");
+for (let i = 0, cnt = cards.length; i < cnt; i++) {
+  const width = cards[i].offsetWidth;
+  cards[i].style.height = width * 0.6 + "px";
+} */
+
+
 const updCards = function (data) {
   main.innerHTML = "";
+
   data.forEach(function (cat) {
     if (cat.id) {
-      let card = `<div class="${cat.favourite ? "card like" : "card"}" style="background-image:
-  url(${cat.img_link || "images/cat.jpg"})">
-  <span>${cat.name}</span>
-  </div>`;
+      let card = `<div class="${cat.favourite ? "card like" : "card"}" style="background-image: url(${cat.img_link || "images/cat.jpg"})">
+      <span>${cat.name}</span>
+      </div>`;
       main.innerHTML += card;
     }
   });
+
   let cards = document.getElementsByClassName("card");
+
   for (let i = 0, cnt = cards.length; i < cnt; i++) {
     const width = cards[i].offsetWidth;
     cards[i].style.height = width * 0.6 + "px";
   }
 }
 
-let cards = document.getElementsByClassName("card");
-for (let i = 0, cnt = cards.length; i < cnt; i++) {
-  const width = cards[i].offsetWidth;
-  cards[i].style.height = width * 0.6 + "px";
+
+let catsData = localStorage.getItem("cats");
+catsData = catsData ? JSON.parse(catsData) : [];
+
+const getCats = function (api, store) {
+  if (!store.length) {
+    api
+      .getCats()
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        if (data.message === "ok") {
+          localStorage.setItem("cats", JSON.stringify(data.data));
+          catsData = [...data.data];
+          updCards(data.data);
+        }
+      })
+  } else {
+    updCards(store);
+  }
 }
+
+getCats(api, catsData);
+
 
 let addBtn = document.querySelector("#add");
 let popupForm = document.querySelector("#popup-form");
 let closePopupForm = popupForm.querySelector(".popup-close");
+
 addBtn.addEventListener("click", (e) => {
   e.preventDefault();
   if (!popupForm.classList.contains("active")) {
@@ -32,12 +75,13 @@ addBtn.addEventListener("click", (e) => {
     popupForm.parentElement.classList.add("active");
   }
 });
+
 closePopupForm.addEventListener("click", () => {
   popupForm.classList.remove("active");
   popupForm.parentElement.classList.remove("active");
 })
 
-const api = new Api("medmixal");
+
 let form = document.forms[0];
 form.img_link.addEventListener("change", (e) => {
   form.firstElementChild.style.backgroundImage = `url(${e.target.value})`
@@ -60,26 +104,41 @@ form.addEventListener("submit", e => {
       }
     }
   }
+
   console.log(body);
-  api.addCat(body)
+
+  api
+    .addCat(body)
     .then(res => res.json())
     .then(data => {
-      if (data.message === "ok") {
-        form.reset();
-        closePopupForm.click();
-      } else {
-        console.log(data);
-      }
-    })
+        if (data.message === "ok") {
+          form.reset();
+          closePopupForm.click();
+          api
+            .getCat(body.id)
+            .then(res => res.json())
+            .then(cat => {
+              if (cat.message === "ok") {
+                catsData.push(cat.data);
+                localStorage.setItem("cats", JSON.stringify(catsData));
+
+            getCats(api, catsData);
+          } else {
+            console.log(cat);
+          }
+        })
+    } else {
+      console.log(data);
+      api
+        .getIds()
+        .then(r => r.json())
+        .then(d => console.log(d));
+    }
+  })
 })
 
-const getCats = function (api) {
-  api.getCats()
-    .then(res => res.json())
-    .then(data => {
-      if (data.message === "ok") {
-        updCards(data.data);
-      }
-    })
-}
-getCats(api);
+
+
+
+
+
